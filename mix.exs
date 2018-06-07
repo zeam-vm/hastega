38465a30a9d2e11d79f66a7d7f4784b319e9bc4c
Susumu Yamazaki <zacky1972@gmail.com>
2018-06-07 14:39:57 +0900

    describe package information

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hastega.MixProject do
   def project do
     [
       app: :hastega,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,6 +23,15 @@ defmodule Hastega.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp package() do
+    [
+      name: "hastega",
+      maintainers: ["Susumu Yamazaki", "Masakazu Mori", "Yoshihiro Ueno", "Hideki Takase"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/zeam-vm/hastega"}
     ]
   end
 end
