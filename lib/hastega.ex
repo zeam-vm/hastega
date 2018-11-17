298af464327819f72a304907423f30d842668d99
zacky1972 <zacky1972@gmail.com>
2018-11-17 09:57:45 +0900

    remove IO.inspect

--- lib/hastega.ex
+++ lib/hastega.ex
@@ -16,7 +16,6 @@ defmodule Hastega do
 
     Stream.iterate(1, &(&1 + 1))
     |> Enum.zip(functions)
-    |> IO.inspect
     |> Enum.map(& :mnesia.dirty_write({:functions, elem(&1, 0), "#{__CALLER__.module}", elem(&1, 1)[:function_name], elem(&1, 1)[:is_public]}))
 
     quote do
