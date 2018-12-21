d84bf0e387823baef6659ea8d235d1b4b7a72040
Susumu Yamazaki <zacky1972@gmail.com>
2018-12-21 20:18:08 +0900

    remove body of generate_nif

--- lib/hastega/generator.ex
+++ lib/hastega/generator.ex
@@ -5,7 +5,6 @@ defmodule Hastega.Generator do
   """
 
   def generate_nif({:functions, id, module_name, function_name, is_public, args, body}) do
-  	
   	{:functions, id, module_name, function_name, is_public, args, body}
   end
 end
