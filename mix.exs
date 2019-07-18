13eb47092203ea4fe906191140e2c8d911c81002
HISA WAY <z8mcb008@gmail.com>
2019-07-18 16:04:42 +0900

    add nif code

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Hastega.MixProject do
       app: :hastega,
       version: "0.0.0",
       elixir: "~> 1.6",
+      compilers: [:nif_vec] ++ Mix.compilers,
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
@@ -23,7 +24,8 @@ defmodule Hastega.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:benchfella, "~> 0.3.5"},
     ]
   end
 
@@ -38,5 +40,19 @@ defmodule Hastega.MixProject do
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/zeam-vm/hastega"}
     ]
+  end
+end
+
+defmodule Mix.Tasks.Compile.NifVec do
+  def run(_) do
+    if match? {:win32, _}, :os.type do
+      # libpostal does not support Windows unfortunately.
+      IO.warn("Windows is not supported.")
+      exit(1)
+    else
+      File.mkdir_p("priv")
+      {result, _error_code} = System.cmd("make", ["priv/libnifvec.so"], stderr_to_stdout: true)
+      IO.binwrite result
+    end
   end
 end
