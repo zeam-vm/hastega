c5d14f7dc07bf363a03605fc80a3e59d0bbc9ea5
HISA WAY <z8mcb008@gmail.com>
2019-08-01 15:47:29 +0900

    update
    add benchmarks for HastegaSample.chunk_every

--- bench/nif_bench.exs
+++ bench/nif_bench.exs
@@ -16,35 +16,47 @@ defmodule HastegaBench do
   #   Enum.map(@list_0x100, & &1 * 2)
   # end
 
-  bench "nif_enum_map_mult_2 0x100" do
-    VecSample.enum_map_mult_2(@list_0x100)
-  end
-
-  # bench "Enum.map 0x1000" do
-  #   Enum.map(@list_0x1000, & &1 * 2)
+  # bench "nif_enum_map_mult_2 0x100" do
+  #   VecSample.enum_map_mult_2(@list_0x100)
   # end
 
-  bench "nif_enum_map_mult_2 0x1000" do
-    VecSample.enum_map_mult_2(@list_0x1000)
-  end
+  # # bench "Enum.map 0x1000" do
+  # #   Enum.map(@list_0x1000, & &1 * 2)
+  # # end
 
-  # bench "Enum.map 0x10000" do
-  #   Enum.map(@list_0x10000, & &1 * 2)
+  # bench "nif_enum_map_mult_2 0x1000" do
+  #   VecSample.enum_map_mult_2(@list_0x1000)
   # end
 
-  bench "enum_map_mult_2 0x10000" do
-    VecSample.enum_map_mult_2(@list_0x10000)
+  # # bench "Enum.map 0x10000" do
+  # #   Enum.map(@list_0x10000, & &1 * 2)
+  # # end
+
+  # bench "enum_map_mult_2 0x10000" do
+  #   VecSample.enum_map_mult_2(@list_0x10000)
+  # end
+
+  # bench "hastega enum_map 0x100" do
+  #   HastegaSample.list_mult_2(@list_0x100)
+  # end
+
+  # bench "hastega enum_map 0x1000" do
+  #   HastegaSample.list_mult_2(@list_0x1000)
+  # end
+
+  # bench "hastega enum_map 0x10000" do
+  #   HastegaSample.list_mult_2(@list_0x10000)
+  # end
+
+  bench "VecSample.chunk_every 0x100" do
+    VecSample.chunk_every(@list_0x100, 4)
   end
 
-  bench "hastega enum_map 0x100" do
-    HastegaSample.list_mult_2(@list_0x100)
+  bench "Hastega.chunk_every 0x100" do
+    HastegaSample.chunk_every(@list_0x100)
   end
 
-  bench "hastega enum_map 0x1000" do
-    HastegaSample.list_mult_2(@list_0x1000)
-  end
-
-  bench "hastega enum_map 0x10000" do
-    HastegaSample.list_mult_2(@list_0x10000)
+  bench "Enum.chunk_every 0x100" do
+    Enum.chunk_every(@list_0x100, 4)
   end
 end
