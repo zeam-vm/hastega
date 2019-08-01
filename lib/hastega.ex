0e78b03a0bc662b9da95d6ff1f266996580c059a
HISA WAY <z8mcb008@gmail.com>
2019-08-01 15:43:20 +0900

    clean sample code
    moved HastegaSample module to sample.ex

--- lib/hastega.ex
+++ lib/hastega.ex
@@ -267,20 +267,3 @@ defmodule Hastega.Func do
     Opt.inspect "This is basic operator :+, but not supported."
   end
 end
-
-defmodule HastegaSample do
-  import Hastega
-  require Hastega
-  defhastega do
-    def list_mult_2(list) do
-      list
-      |> Enum.map(& &1 * 2)
-      # |> Enum.map(fn x -> x * 3 end)
-    end
-
-    def chunk_every(list) do
-      list
-      |> Enum.chunk_every(4)
-    end
-  end
-end
